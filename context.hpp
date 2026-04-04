ebd763ada7f2bc5fab643ff64e6040f4746dd079
eric <210752068@qq.com>
2026-04-04 15:07:20 +0800

    [chore] 创建并拿到命令队列

--- context.hpp
+++ context.hpp
@@ -23,6 +23,11 @@ namespace hf {
 class Context {
   struct QueueFamilyIndices {
     std::optional<int64_t> graphics_family;
+    std::optional<int64_t> present_family;
+
+    operator bool() const {
+      return graphics_family.has_value() && present_family.has_value();
+    }
   };
 
 public:
@@ -44,17 +49,19 @@ public:
     assert(instance != VK_NULL_HANDLE);
     phy_device = pickup_physical_device(instance);
     assert(phy_device != VK_NULL_HANDLE);
-    que_family_indices =
-        query_family_indices(phy_device, vk::QueueFlagBits::eGraphics);
-    assert(que_family_indices.graphics_family.has_value());
 
     surface = fn(instance);
     assert(surface != VK_NULL_HANDLE);
 
-    device = create_device(phy_device);
+    que_family_indices =
+        query_family_indices(phy_device, surface, vk::QueueFlagBits::eGraphics);
+
+    device = create_device(phy_device, que_family_indices);
     assert(device != VK_NULL_HANDLE);
     auto graphic_que =
         device.getQueue(que_family_indices.graphics_family.value(), 0);
+    auto present_que =
+        device.getQueue(que_family_indices.present_family.value(), 0);
   }
 
   ~Context() {
@@ -94,19 +101,34 @@ private:
     return devices[0];
   }
 
-  static vk::Device create_device(const vk::PhysicalDevice &phy_device) {
+  static vk::Device
+  create_device(const vk::PhysicalDevice &phy_device,
+                const QueueFamilyIndices &que_family_indices) {
+    assert(que_family_indices);
+
+    bool is_same_family = que_family_indices.graphics_family.value() ==
+                          que_family_indices.present_family.value();
+
     vk::DeviceCreateInfo create_info;
-    vk::DeviceQueueCreateInfo que_create_info;
+    std::vector<vk::DeviceQueueCreateInfo> que_create_infos;
+    que_create_infos.reserve(is_same_family ? 1 : 2);
+
     float que_priority = 1.0f;
-
-    auto graphic_family_indices =
-        query_family_indices(phy_device, vk::QueueFlagBits::eGraphics);
-    assert(graphic_family_indices.graphics_family.has_value());
-    que_create_info.setPQueuePriorities(&que_priority)
+    vk::DeviceQueueCreateInfo graphic_que_create_info;
+    graphic_que_create_info.setPQueuePriorities(&que_priority)
         .setQueueCount(1)
-        .setQueueFamilyIndex(graphic_family_indices.graphics_family.value());
+        .setQueueFamilyIndex(que_family_indices.graphics_family.value());
+    que_create_infos.push_back(std::move(graphic_que_create_info));
 
-    create_info.setQueueCreateInfos(que_create_info);
+    if (!is_same_family) {
+      vk::DeviceQueueCreateInfo present_queue_create_info;
+      present_queue_create_info.setPQueuePriorities(&que_priority)
+          .setQueueCount(1)
+          .setQueueFamilyIndex(que_family_indices.present_family.value());
+      que_create_infos.push_back(std::move(present_queue_create_info));
+    }
+
+    create_info.setQueueCreateInfos(que_create_infos);
     return phy_device.createDevice(create_info);
   }
 
@@ -120,6 +142,24 @@ private:
         indices.graphics_family = i;
         break;
       }
+    }
+    return indices;
+  }
+
+  static QueueFamilyIndices
+  query_family_indices(const vk::PhysicalDevice &phy_device,
+                       vk::SurfaceKHR surface, vk::QueueFlagBits flags) {
+    QueueFamilyIndices indices;
+    auto queue_families = phy_device.getQueueFamilyProperties();
+    for (int64_t i = 0; i < static_cast<int64_t>(queue_families.size()); i++) {
+      if (queue_families[i].queueFlags & flags) {
+        indices.graphics_family = i;
+      }
+      if (phy_device.getSurfaceSupportKHR(i, surface)) {
+        indices.present_family = i;
+      }
+      if (indices)
+        break;
     }
     return indices;
   }
